97f25d7772aff83fe4f27afeae969485fb90e939
dren79 <davidleerenton@gmail.com>
2015-12-28 18:05:38 +0000

    Update Program.cs

--- DatabaseSQLtoMSSQL/Program.cs
+++ DatabaseSQLtoMSSQL/Program.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using MySql.Data.MySqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 
 namespace DatabaseSQLtoMSSQL
@@ -13,45 +8,34 @@ namespace DatabaseSQLtoMSSQL
     {
         static void Main(string[] args)
         {
-            string mssqlConnection = "Server ="server address"; Database ="database name"; Uid ="user name"; Pwd ="password"";
+            // ms sql connection string 
+            string mssqlConnection = "Server =server_address; Database =DBName; Uid =UserName; Pwd =Password";
             SqlConnection mssqlConn = new SqlConnection(mssqlConnection);
             mssqlConn.Open();
-
-            string ConnectionString = "Server ="server address"; Database ="database name"; Uid ="user name"; Pwd ="password"";
+            // mysql connectioin string
+            string ConnectionString = "Server =server_address; Database =DBName; Uid =UserName; Pwd =Password";
             MySqlConnection conn = new MySqlConnection(ConnectionString);
             conn.Open();
+
+            // go get the information from the mySql table
             MySqlCommand myCommand = new MySqlCommand("select * from AllRelVar", conn);
-            //MySqlDataReader myReader = myCommand.ExecuteReader();
-                    //while (myReader.Read())
-                    //{
-                        DataTable table = new DataTable();
-                        table.Load(myCommand.ExecuteReader());
-                        //int id = Convert.ToInt32(myReader["id"]);
-                        //int relid = Convert.ToInt32(myReader["Rel_ID"]);
-                        //string rel = myReader["Religion"].ToString();
-                        //SqlCommand mssqlCommand = new SqlCommand("INSERT INTO ReligionsAll VALUES(@id, @relID, @rel)", mssqlConn);
-                        //mssqlCommand.Parameters.AddWithValue("@id",id);
-                        //mssqlCommand.Parameters.AddWithValue("@relID",relid);
-                        //mssqlCommand.Parameters.AddWithValue("@rel",rel);
-                        //mssqlCommand.ExecuteNonQuery();
-                        //Console.WriteLine(id);
-                   // }
-                        //using (mssqlConn)
-                        using (SqlBulkCopy bulk = new SqlBulkCopy(mssqlConn))
-                        {
-                            bulk.DestinationTableName = "ReligionsAll";
-                            bulk.WriteToServer(table);
-                        }
-                        Console.WriteLine(table.ToString());
-                    mssqlConn.Close();
-                    conn.Close();
-                //SqlDataAdapter adp = new SqlDataAdapter("select * from Religions", ConnectionString);
-                //DataSet ds = new DataSet();
-                //try { adp.Fill(ds, "ReligionsTable"); }
-                //catch (Exception ex) {
-                //    Console.Out.WriteLine(ex);
-                //}
-                //Console.Out.WriteLine(ds.ToString());
+
+            // make a data table to hold the info
+            DataTable table = new DataTable();
+            // load the query results into the table
+            table.Load(myCommand.ExecuteReader());
+
+            // copy the table to its new home in the ms sql table
+            using (SqlBulkCopy bulk = new SqlBulkCopy(mssqlConn))
+            {
+                // tell it which table to copy to
+                bulk.DestinationTableName = "ReligionsAll";
+                // copy the info
+                bulk.WriteToServer(table);
+            }
+            // close both connections
+            mssqlConn.Close();
+            conn.Close();
         }
     }
 }
